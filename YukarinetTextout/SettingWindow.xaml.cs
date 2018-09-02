a7e99e2299d123fcc5818ad7df60b4475ef2ff8e
Gniang <jing.art@gmail.com>
2018-09-03 02:30:57 +0900

    0.0.1

---
+++ YukarinetTextout/SettingWindow.xaml.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Win32;
+
+namespace YukarinetTextout
+{
+    /// <summary>
+    /// Setting.xaml の相互作用ロジック
+    /// </summary>
+    public partial class Setting : Window
+    {
+        /// <summary>
+        /// Initialize parameter
+        /// </summary>
+        internal struct InitParam { 
+            /// <summary>
+            /// OutputPath
+            /// </summary>
+            internal string Path { get; set; }
+        }
+
+        public Setting()
+        {
+            InitializeComponent();
+        }
+
+
+        public string GetOutputPath()
+        {
+            return txtPath.Text;
+        }
+
+        /// <summary>
+        /// Initialize
+        /// </summary>
+        /// <param name="p"></param>
+        internal void Init(InitParam p)
+        {
+            txtPath.Text = p.Path;
+        }
+
+        
+
+        private void btnDialog_Click(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.FilterIndex = 1;
+            saveFileDialog.Filter = "Text Files(.txt)|*.txt|All Files (*.*)|*.*";
+            bool? result = saveFileDialog.ShowDialog();
+            if (result == true)
+            {
+                txtPath.Text = saveFileDialog.SafeFileName;
+            }
+        }
+
+        private void btnOk_Click(object sender, RoutedEventArgs e)
+        {
+
+            try
+            {
+                var fullPath = System.IO.Path.GetFullPath(this.GetOutputPath());
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Invalid file path. パスが不正です。");
+                return;
+            }
+
+            DialogResult = true;
+        }
+    }
+}
---
+++ YukarinetTextout/Textout.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xaml;
+using System.Xml;
+using System.Xml.Serialization;
+using Yukarinette;
+
+namespace YukarinetTextout
+{
+    /// <summary>
+    /// InputVoice to Text
+    /// </summary>
+    public class Textout:IYukarinetteInterface
+    {
+        private string appLocalDataPath;
+
+        private const string settingFileName = "TextOut.config";
+
+        private SettingData settingData;
+
+
+        /// <summary>PluginName </summary>
+        public override string Name { get { return "TextOut"; } }
+        
+
+        public Textout()
+        {
+        }
+
+        public override void Loaded()
+        {
+            base.Loaded();
+            try
+            {
+                var local = System.Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+                appLocalDataPath = Path.Combine(local, @"Yukarinette\plugins\");
+                settingData = ReadSetting();
+            }
+            catch (Exception ex)
+            {
+                throw new YukarinetteException(ex);
+            }
+        }
+
+        /// <summary>OnSetting </summary>
+        /// <remarks>Show dialog and Setting save path</remarks>
+        public override void Setting()
+        {
+            base.Setting();
+
+            EditSetting();
+        }
+
+
+
+
+        /// <summary> </summary>
+        public override void AfterSpeech(string text)
+        {
+            base.AfterSpeech(text);
+
+            AppendText(text, this.settingData.Path);
+        }
+
+        private void AppendText(string text, string path)
+        {
+            using (var sw = new StreamWriter(path, true, Encoding.UTF8))
+            {
+                sw.WriteLine(text);
+            }
+        }
+
+
+        private SettingData ReadSetting()
+        {
+            var path = Path.Combine(appLocalDataPath, settingFileName);
+            if (File.Exists(path))
+            {
+                var serializer = new XmlSerializer(typeof (SettingData));
+                using (var reader = XmlReader.Create(path))
+                {
+                    return (SettingData) serializer.Deserialize(reader);
+                }
+            }
+            else
+            {
+                var outPath = GetDefaultOutputPath();
+                return new SettingData()
+                {
+                    Path = outPath,
+                };
+            }
+        }
+
+        private void EditSetting()
+        {
+            var f = new Setting();
+            f.Init(new Setting.InitParam() { Path = this.settingData.Path });
+            bool? result = f.ShowDialog();
+            if (result == true)
+            {
+                var outPath = f.GetOutputPath();
+                if (!string.IsNullOrWhiteSpace(outPath))
+                {
+                    this.settingData.Path = outPath;
+                    this.SaveSetting();
+                }
+            }
+        }
+
+        private void SaveSetting()
+        {
+            var path = Path.Combine(appLocalDataPath, settingFileName);
+            var serializer = new XmlSerializer(typeof(SettingData));
+            using (var sw = XmlWriter.Create(path))
+            {
+                serializer.Serialize(sw, this.settingData ?? new SettingData());
+            }
+        }
+
+        private string GetDefaultOutputPath()
+        {
+            return Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory),
+                                            "textout.txt");
+        }
+    }
+}
